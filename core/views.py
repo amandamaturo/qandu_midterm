ddbfbe669804d33491fb17bd8225a3c13a08ee42
Amanda <maturo.amanda@gmail.com>
2015-10-22 19:32:10 +0000

    Add message create view

--- core/views.py
+++ core/views.py
@@ -4,3 +4,13 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
   template_name = "home.html"
+
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
+
+class MessageCreateView(CreateView):
+  model = Message
+  template_name = "message/message_form.html"
+  fields = ['title', 'description']
+  success_url = reverse_lazy('home')
